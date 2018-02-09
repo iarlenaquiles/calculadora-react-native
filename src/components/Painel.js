8e7de10bc959abf640227deb14fba949daf08213
Iarlen <iarlensilva775@gmail.com>
2018-02-09 16:14:34 -0300

    Importando os componentes

--- src/components/Painel.js
+++ src/components/Painel.js
@@ -1,5 +1,9 @@
- import React from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
+
+import Entrada from './Entrada';
+import Operacao from './Operacao';
+import Comando from './Comando';
 
 const Painel = props => (
   		<View>
