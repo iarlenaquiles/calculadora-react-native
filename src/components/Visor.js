1038a3788132811f497cdb1948fbffe3387f2fc4
Iarlen <iarlensilva775@gmail.com>
2018-02-09 15:40:51 -0300

    Importando o text input

--- src/components/Visor.js
+++ src/components/Visor.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import {View, Text, TextInput } from 'react-native';
+import {View, TextInput } from 'react-native';
 
  export default props => (
  	<View>
- 		<Text>Visor</Text>
+ 		<TextInput />
  	</View>
  );
